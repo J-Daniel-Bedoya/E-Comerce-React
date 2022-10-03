98d20cd0823a04a6b97394deaa8bf1b6b990f294
owenunda <oweenunda@gmail.com>
2022-10-03 16:47:55 -0500

    se arreglo lo que decia abajo del button.

--- src/components/LoginUp.jsx
+++ src/components/LoginUp.jsx
@@ -18,11 +18,11 @@ const LoginUp = () => {
         <form action="" className='loginUp__card--form'>
           <div className='loginUp__card--input'>
             <label htmlFor="email"><i className="fa-solid fa-envelope"></i></label>
-            <input id='email' type="email" />
+            <input id='email' placeholder="example@gmail.com" type="email" />
           </div>
           <div className='loginUp__card--input'>
             <label htmlFor="password"><i className="fa-solid fa-lock"></i></label>
-            <input id='password' type={isVisible ? "password" : "text"}/>
+            <input id='password'placeholder="***********"  type={isVisible ? "password" : "text"}/>
             <div onClick={() => setIsVisible(!isVisible)} className="isVisible">
               {
                 isVisible ? <i className="fa-regular fa-eye-slash"></i> : <i className="fa-regular fa-eye"></i>
@@ -33,7 +33,7 @@ const LoginUp = () => {
             <button>Login Up</button>
           </div>
         </form>
-        <p className='loginUp__cart--footerForm'>Ya tienes cuenta? <Link className='Link' to={"/login/SignUp"}>Sign Up</Link></p>
+        <p className='loginUp__cart--footerForm'>Don't have account? <Link className='Link' to={"/login/SignUp"}>Sign Up</Link></p>
       </div>
     </div>
   );
