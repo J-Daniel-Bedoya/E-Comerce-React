17759da45956f4796d30356fbbc9086a5d1b4123
daniel1718 <jbedoyachavarriaga@gmail.com>
2022-10-05 21:26:37 -0500

    cart syles finaly

--- src/store/slices/purchases.slice.js
+++ src/store/slices/purchases.slice.js
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
 import getConfig from '../../utils/getConfig';
 
 export const purchasesSlice = createSlice({
@@ -14,11 +13,8 @@ export const purchasesSlice = createSlice({
 })
 
 export const purchasesThunk = () => (dispatch) => {
-    //dispatch(setIsLoading(true));
-    //const token = localStorage.getItem("token")
     return axios.get("https://ecommerce-api-react.herokuapp.com/api/v1/purchases", getConfig())
         .then((res) => dispatch(purchasesSet(res.data.data.purchases)))
-        //.finally(() => dispatch(setIsLoading(false)));
 }
 
 export const { purchasesSet } = purchasesSlice.actions;
